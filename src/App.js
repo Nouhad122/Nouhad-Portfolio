561b0707af19f02569dccaee97e2a8322cf5dca5
Nouhad122 <nouhadalhallab122@gmail.com>
2024-10-30 17:39:32 +0300

    Uploading my portfolio

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import About from "./components/About";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
@@ -9,7 +9,15 @@ import './style.css';
 import Footer from './components/Footer';
 
 function App() {
-  const [darkMode,setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(() => {
+    // Retrieve the saved mode from localStorage, default to false if not set
+    return JSON.parse(localStorage.getItem('darkMode')) || false;
+  });
+
+  useEffect(() => {
+    // Save the mode to localStorage whenever darkMode changes
+    localStorage.setItem('darkMode', JSON.stringify(darkMode));
+  }, [darkMode]);
   return (
     <div className={`${darkMode ? 'App dark-mode' : 'App light-mode'}`}>
       <Navbar darkMode = {darkMode} setDarkMode={setDarkMode}/>
