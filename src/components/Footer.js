b80378feb75c6d7d5ff3bdce6c67251775824d11
Nouhad122 <nouhadalhallab122@gmail.com>
2024-10-31 11:08:22 +0300

    fixed some anomalies

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import '../components/Footer.css'
 const Footer = ({darkMode}) => {
   return (
     <>
-      <footer className={darkMode && 'dark-footer'}>
+      <footer className={darkMode ?'dark-footer' : ''}>
         <h2>Nouhad El Hallab - 2003</h2>
       </footer>
     </>
